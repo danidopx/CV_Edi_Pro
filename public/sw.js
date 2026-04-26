7637fb567095ef278a4b9254a3d4a2935751db25
GitHub Action <action@github.com>
2026-04-26 16:17:53 +0000

    chore: v0.0.45 [skip ci]

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 /** Em push na main, o workflow GitHub substitui este valor pelo CACHE_ID (igual ao ?v= do index.html). */
-const CACHE_NAME = 'cv-edi-pro-v19905';
+const CACHE_NAME = 'cv-edi-pro-v20272';
 
 const PRECACHE_URLS = [
     '/',
